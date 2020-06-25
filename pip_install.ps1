f759090318c022bd8c7c3ade5929b9b48df128c4
Jacob Holman <jdholman9@gmail.com>
2020-06-25 10:49:44 -0700

    Added comments

--- pip_install.ps1
+++ pip_install.ps1
@@ -3,12 +3,19 @@
 #  .\pip_install.ps1 if the file is in the current directory
 # Currently file path is "C:\Users\jholman\Documents\Programming\Startup\pip_install.ps1"
 # Also can be run by right clicking and run in Powershell
+
+# Control mouse and keyboard
 pip install pyautogui
+# Data handling
 pip install pandas
+# package required by pandas but not included
 pip install xlrd
+# package for working with excel
 pip install openpyxl
+# for jupyter notebooks
 pip install jupyter
 
+# extra stuff because it is cool
 Add-Type -AssemblyName System.speech
 $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
 
